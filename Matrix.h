8c4ef1b18bc52709da5239413700dc13adbe0970
malul88 <malul88>
2020-06-30 22:07:38 +0300

    documentation

--- Matrix.h
+++ Matrix.h
@@ -170,7 +170,18 @@ namespace mtm {
             return printMatrix(os, begin, end, matrix1.width());
         }
 
-
+    /**
+     * operator+ - adds two matrix (same dimensions)
+     * @param matrix1
+     * @param matrix2
+     * @return added matrix
+     * T must have:
+     * - default c'tor
+     * - copy c'tor
+     * - operator+()
+     * - operator+=()
+     * -
+     */
         friend Matrix<T> operator+(const Matrix<T> &matrix1, const Matrix<T> &matrix2) {
             if (matrix1.size() == 1) {
                 Matrix<T> new_mat = matrix2;
@@ -206,14 +217,19 @@ namespace mtm {
     };
 
 
-//    template<class T>
-//    Matrix<T> operator+(const Matrix<T> &matrix1, const Matrix<T> &matrix2)
+    /**
+     * check if there's any true element in the matrix
+     * @tparam T
+     * @param matrix
+     * @return true if one (at least) of the element is true otherwise - flase
+     *
+     */
 
     template<class T>
     bool any(const Matrix<T> &matrix) {
         for (int i = 0; i < matrix.height(); ++i) {
             for (int j = 0; j < matrix.width(); ++j) {
-                if (static_cast<bool>(matrix(i, j))) { // todo check if it works
+                if (static_cast<bool>(matrix(i, j))) {
                     return true;
                 }
             }
@@ -221,6 +237,12 @@ namespace mtm {
         return false;
     }
 
+    /**
+     *
+     * @tparam T
+     * @param matrix
+     * @return
+     */
     template<class T>
     bool all(const Matrix<T> &matrix) {
         for (int i = 0; i < matrix.height(); ++i) {
@@ -233,12 +255,16 @@ namespace mtm {
         return true;
     }
 
-/** Matrix constructor
- * initialize the fields and allocate new matrix with the desired type
- * @tparam T - The type which the matrix is based on
- * @param dims - The dimension of the matrix has to be Dimension type.
- * @param initial - The same type of T.
- */
+    /** Matrix constructor
+     * initialize the fields and allocate new matrix with the desired type
+     * @tparam T - The type which the matrix is based on
+     * @param dims - The dimension of the matrix has to be Dimension type.
+     * @param initial - The same type of T.
+     * * T must have:
+     * - default c'tor
+     * - copy c'tor
+     * - operator=()
+     */
     template<class T>
     Matrix<T>::Matrix(Dimensions dims, T initial) : matrix(nullptr), dimensions(dims) {
         //verifyDimension(dims);
@@ -260,11 +286,15 @@ namespace mtm {
 
     }
 
-/**
- * constructor which create a (1,1) dimension matrix with the initial T.
- * @tparam T - The desired type.
- * @param initial - The initial T which will be the only element of the matrix.
- */
+    /**
+     * constructor which create a (1,1) dimension matrix with the initial T.
+     * @tparam T - The desired type.
+     * @param initial - The initial T which will be the only element of the matrix.
+     * * T must have:
+     * - default c'tor
+     * - copy c'tor
+     * - operator=()
+     */
     template<class T>
     Matrix<T>::Matrix(T initial) : matrix(nullptr), dimensions(1, 1) {
         int row = 1;
@@ -280,12 +310,16 @@ namespace mtm {
         }
     }
 
-/**
- * copy constructor - Initial matrix with the same data of other matrix , allocate a new memory
- * for the new matrix.
- * @tparam T - The desired type.
- * @param matrix1 - The matrix which will initialized with (from the same type).
- */
+    /**
+     * copy constructor - Initial matrix with the same data of other matrix , allocate a new memory
+     * for the new matrix.
+     * @tparam T - The desired type.
+     * @param matrix1 - The matrix which will initialized with (from the same type).
+     * * T must have:
+     * - default c'tor
+     * - copy c'tor
+     * - operator=()
+     */
     template<class T>
     Matrix<T>::Matrix(const Matrix &matrix1) : matrix(nullptr), dimensions(matrix1.dimensions) {
         int row = dimensions.getRow();
@@ -303,10 +337,10 @@ namespace mtm {
         }
     }
 
-/**
- * d'tor - Free al the memory allocated in the object.
- * @tparam T - The desired type.
- */
+    /**
+     * d'tor - Free al the memory allocated in the object.
+     * @tparam T - The desired type.
+     */
     template<class T>
     Matrix<T>::~Matrix() {
         int row = this->dimensions.getRow();
@@ -315,12 +349,16 @@ namespace mtm {
         delete[] matrix;
     }
 
-/**
- * operator= - assigment operator create a new matrix equals to matrix 1 and replace it with 'this' matrix.
- * @tparam T - The desired type.
- * @param matrix1 - matrix typed T.
- * @return - Matrix which equals to matrix1.
- */
+    /**
+     * operator= - assigment operator create a new matrix equals to matrix 1 and replace it with 'this' matrix.
+     * @tparam T - The desired type.
+     * @param matrix1 - matrix typed T.
+     * @return - Matrix which equals to matrix1.
+     * * T must have:
+     * - default c'tor
+     * - copy c'tor
+     * - operator=()
+     */
     template<class T>
     Matrix<T> &Matrix<T>::operator=(const Matrix &matrix1) {
         int row = matrix1.dimensions.getRow();
@@ -341,6 +379,11 @@ namespace mtm {
         return *this;
     }
 
+    /**
+     * check if the dimension are correct if not - throwing exception
+     * @tparam T
+     * @param dims - The dimensions for checking.
+     */
     template<class T>
     void Matrix<T>::verifyDimension(Dimensions dims) {
         if (dims.getCol() <= 0 || dims.getRow() <= 0) {
@@ -348,6 +391,17 @@ namespace mtm {
         }
     }
 
+    /**
+     * Diagonal - create diagonal matrix with the initial t
+     * @tparam T - initial
+     * @param dim - squared dim
+     * @param t
+     * @param initial
+     * @return diagonal matrix
+     * * T must have:
+     * - default c'tor
+     * - operator=()
+     */
     template<class T>
     Matrix<T> Matrix<T>::Diagonal(int dim, T t, T initial) {
         if (dim <= 0) {
@@ -360,25 +414,38 @@ namespace mtm {
         }
         return result;
     }
-
+    /**
+     * return the number of rows in the matrix
+     */
     template<class T>
     int Matrix<T>::height() const {
         int result = dimensions.getRow();
         return result;
     }
-
+    /**
+     * return the number of rows in the matrix
+     */
     template<class T>
     int Matrix<T>::width() const {
         int result = dimensions.getCol();
         return result;
     }
-
+    /**
+     * return the number of elements in the matrix
+     */
     template<class T>
     int Matrix<T>::size() const {
         int result = (this->height()) * (this->width());
         return result;
     }
-
+    /**
+     * create transposed matrix of the matrix sent as "this"
+     * @tparam T = the typename
+     * @return tranposed matrix<T>
+     * * T must have:
+     * - default c'tor
+     * - operator=()
+     */
     template<class T>
     Matrix<T> Matrix<T>::transpose() const {
         Dimensions transposed_dimensions(this->dimensions.getCol(), this->dimensions.getRow());// in purpose in order to make transposed matrix
@@ -394,6 +461,15 @@ namespace mtm {
     }
 
     template<class T>
+    /**
+     * operator-() - create matrix where all the elements are the opposite of 'this' matrix
+     * @tparam T - the typename
+     * @return - the created matrix
+     * * T must have:
+     * - default c'tor
+     * - operator=()
+     * - operator-()
+     */
     Matrix<T> Matrix<T>::operator-() const {
         Matrix<T> result(this->dimensions);
         int col = this->dimensions.getCol();
@@ -405,7 +481,16 @@ namespace mtm {
         }
         return result;
     }
-
+    /**
+     * Binary operator. return new matrix equals to mat1 - mat2
+     * @tparam T - the typename
+     * @param matrix1
+     * @return new matrix
+     * * T must have:
+     * - default c'tor
+     * - operator=()
+     * - operator-()
+     */
     template<class T>
     Matrix<T> Matrix<T>::operator-(const Matrix matrix1) const {
         if (this->dimensions != matrix1.dimensions) {
@@ -415,6 +500,12 @@ namespace mtm {
         return result;
     }
 
+    /**
+     * operator+=() - add t to all the elements in 'this' matrix
+     * * T must have:
+     * - default c'tor
+     * - operator+=()
+     */
     template<class T>
     Matrix<T> &Matrix<T>::operator+=(const T t) {
         int row = this->dimensions.getRow();
@@ -427,7 +518,16 @@ namespace mtm {
         return *this;
     }
 
-
+    /**
+     * operator() - return const specific element in 'this' matrix
+     * @tparam T - the typename
+     * @param row
+     * @param col
+     * @return specific element
+     * * T must have:
+     * - default c'tor
+     * - default copy c'tor
+     */
     template<class T>
     const T Matrix<T>::operator()(const int row, const int col) const {
         if (col < 0 || col >= this->dimensions.getCol() || row < 0 || row >= this->dimensions.getRow()) {
@@ -437,6 +537,15 @@ namespace mtm {
         return result;
     }
 
+    /**
+     * operator() - return non-const specific element in 'this' matrix
+     * @tparam T - the typename
+     * @param row
+     * @param col
+     * @return non-const specific element
+     * * T must have:
+     * - default c'tor
+     */
     template<class T>
     T &Matrix<T>::operator()(const int row, const int col) {
         if (col < 0 || col >= this->dimensions.getCol() || row < 0 || row >= this->dimensions.getRow()) {
@@ -444,7 +553,14 @@ namespace mtm {
         }
         return this->matrix[row][col];
     }
-
+    /**
+     * operator< - compare all the matrix element with the 'n'
+     * @tparam T
+     * @param n
+     * @return - return the result in boolean matrix
+     *  T must have:
+     * - operator<
+     */
     template<class T>
     Matrix<bool> Matrix<T>::operator<(T n) const {
         Matrix<bool> result(this->dimensions, false);
@@ -460,7 +576,14 @@ namespace mtm {
         return result;
 
     }
-
+    /**
+     * operator> - compare all the matrix element with the 't'
+     * @tparam T
+     * @param t
+     * @return - return the result in boolean matrix
+     *  T must have:
+     * - operator>
+     */
     template<class T>
     Matrix<bool> Matrix<T>::operator>(T t) const {
         Matrix<bool> result(this->dimensions, false);
@@ -476,6 +599,14 @@ namespace mtm {
         return result;
     }
 
+    /**
+   * operator<= - compare all the matrix element with the 't'
+   * @tparam T
+   * @param t
+   * @return - return the result in boolean matrix
+   *  T must have:
+   * - operator<=
+   */
     template<class T>
     Matrix<bool> Matrix<T>::operator<=(T t) const {
         Matrix<bool> result(this->dimensions, false);
@@ -491,6 +622,14 @@ namespace mtm {
         return result;
     }
 
+    /**
+   * operator> - compare all the matrix element with the 't'
+   * @tparam T
+   * @param t
+   * @return - return the result in boolean matrix
+   *  T must have:
+   * - operator>=
+   */
     template<class T>
     Matrix<bool> Matrix<T>::operator>=(T t) const {
         Matrix<bool> result(this->dimensions, false);
@@ -505,7 +644,14 @@ namespace mtm {
         }
         return result;
     }
-
+    /**
+     * operator== - compare all the matrix element with the 't'
+     * @tparam T
+     * @param n
+     * @return - return the result in boolean matrix
+     *  T must have:
+     * - operator==
+     */
     template<class T>
     Matrix<bool> Matrix<T>::operator==(T t) const {
         Matrix<bool> result(this->dimensions, false);
@@ -521,6 +667,14 @@ namespace mtm {
         return result;
     }
 
+    /**
+     * operator< - compare all the matrix element with the 't'
+     * @tparam T
+     * @param t
+     * @return - return the result in boolean matrix
+     *  T must have:
+     * - operator!=
+     */
     template<class T>
     Matrix<bool> Matrix<T>::operator!=(T t) const {
         Matrix<bool> result(this->dimensions, false);
@@ -537,14 +691,38 @@ namespace mtm {
     }
 
 
+    /**
+     * iterator constructor
+     * @tparam T
+     * @param dimensions
+     * @param index
+     * @param matrix
+     * T must have:
+     * - copy c'tor
+     */
     template<class T>
     Matrix<T>::iterator::iterator(mtm::Dimensions dims, int i, T **matrix) : dims(dims), index(i), matrix(matrix) {
     }
 
+    /**
+     * iterator copy constructor
+     * @tparam T
+     * @param dimensions
+     * @param index
+     * @param matrix
+     * T must have:
+     * - copy c'tor
+     */
     template<class T>
     Matrix<T>::iterator::iterator(const Matrix::iterator &it) :  dims(it.dims), index(it.index), matrix(it.matrix) {
     }
 
+    /**
+     * iterator operator=
+     * @tparam iterator
+     * @param iterator
+     * @return the same iterator with the 'it' value
+     */
     template<class T>
     typename Matrix<T>::iterator &Matrix<T>::iterator::operator=(const Matrix::iterator &it) {
         if (this == &it) {
@@ -556,6 +734,10 @@ namespace mtm {
         return *this;
     }
 
+    /**
+     * iterator operator*
+     * @return the matrix element which the iterator is point at
+     */
     template<class T>
     T &Matrix<T>::iterator::operator*() const {
         if (index < 0 || index >= (dims.getCol() * dims.getRow())) {
@@ -565,13 +747,19 @@ namespace mtm {
         int row = index / dims.getCol();
         return matrix[row][column];
     }
-
+    /**
+     * iterator operator++
+     * @return iterator point the next matrix element
+     */
     template<class T>
     typename Matrix<T>::iterator Matrix<T>::iterator::operator++() {
         index++;
         return *this;
     }
-
+    /**
+     * iterator operator++ - '++iterator'
+     * @return iterator point the next matrix element
+     */
     template<class T>
     typename Matrix<T>::iterator Matrix<T>::iterator::operator++(int) {
         iterator it = *this;
@@ -579,11 +767,21 @@ namespace mtm {
         return it;
     }
 
+    /**
+     * iterator operator==
+     * @param iterator
+     * @return the result as boolean
+     */
     template<class T>
     bool Matrix<T>::iterator::operator==(const Matrix::iterator &it) const {
         return !operator!=(it);
     }
 
+    /**
+     * iterator operator!=
+     * @param iterator
+     * @return the result as boolean
+     */
     template<class T>
     bool Matrix<T>::iterator::operator!=(const Matrix::iterator &it) const {
         bool b = ((dims != it.dims) || (index != it.index) || (matrix != it.matrix));
@@ -591,17 +789,39 @@ namespace mtm {
     }
 
 
-    ///const iterator implement:
+    /**
+     * conat iterator constructor
+     * @tparam T
+     * @param dimensions
+     * @param index
+     * @param matrix
+     * T must have:
+     * - copy c'tor
+     */
     template<class T>
     Matrix<T>::const_iterator::const_iterator(Dimensions dims, int i, T **matrix)
             : dims(dims), index(i), matrix(matrix) {
     }
 
+    /**
+     * const iterator copy constructor
+     * @tparam T
+     * @param dimensions
+     * @param index
+     * @param matrix
+     * T must have:
+     * - copy c'tor
+     */
     template<class T>
     Matrix<T>::const_iterator::const_iterator(const Matrix::const_iterator &it)
             : dims(it.dims), index(it.index), matrix(it.matrix) {
     }
 
+    /**
+     * const iterator operator=
+     * @tparam iterator
+     * @return the same const iterator with the 'it' value
+     */
     template<class T>
     typename Matrix<T>::const_iterator &Matrix<T>::const_iterator::operator=(const Matrix::const_iterator &it) {
         if (this == &it) {
@@ -612,7 +832,10 @@ namespace mtm {
         index = it.index;
         return *this;
     }
-
+    /**
+     * const iterator operator*
+     * @return the matrix element which the const iterator is point at
+     */
     template<class T>
     const T Matrix<T>::const_iterator::operator*() const {
         if (index < 0 || index >= (dims.getCol() * dims.getRow())) {
@@ -623,56 +846,92 @@ namespace mtm {
         const T t = matrix[row][column];
         return t;
     }
-
+    /**
+     * const iterator operator++
+     * @return const iterator point the next matrix element
+     */
     template<class T>
     typename Matrix<T>::const_iterator Matrix<T>::const_iterator::operator++() {
         this->index++;
         return *this;
     }
-
+    /**
+     * const iterator operator++ - '++iterator'
+     * @return const iterator point the next matrix element
+     */
     template<class T>
     typename Matrix<T>::const_iterator Matrix<T>::const_iterator::operator++(int) {
         const_iterator it = *this;
         this->index++;
         return it;
     }
-
+    /**
+     * const iterator operator==
+     * @param iterator
+     * @return the result as boolean
+     */
     template<class T>
     bool Matrix<T>::const_iterator::operator!=(const Matrix::const_iterator &it) const {
         bool b = ((dims != it.dims) || (index != it.index) || (matrix != it.matrix));
         return b;
     }
-
+    /**
+     * const iterator operator!=
+     * @param iterator
+     * @return the result as boolean
+     */
     template<class T>
     bool Matrix<T>::const_iterator::operator==(const Matrix::const_iterator &it) const {
         return !operator!=(it);
     }
 
-
+    /**
+     * iterator - begin
+     * @tparam T
+     * @return iterator which point at the first matrix element
+     */
     template<class T>
     typename Matrix<T>::iterator Matrix<T>::begin() {
         iterator it(this->dimensions, 0, this->matrix);
         return it;
     }
 
+    /**
+     * iterator - end
+     * @tparam T
+     * @return iterator which point at the matrix element one past after the last elemenent
+     */
     template<class T>
     typename Matrix<T>::iterator Matrix<T>::end() {
         iterator it(this->dimensions, dimensions.getCol() * dimensions.getRow(), this->matrix);
         return it;
     }
-
+    /**
+     * const iterator - begin
+     * @tparam T
+     * @return const iterator which point at the first matrix element
+     */
     template<class T>
     typename Matrix<T>::const_iterator Matrix<T>::begin() const {
         const_iterator it(this->dimensions, 0, this->matrix);
         return it;
     }
-
+    /**
+     * const iterator - end
+     * @tparam T
+     * @return const iterator which point at the matrix element one past after the last elemenent
+     */
     template<class T>
     typename Matrix<T>::const_iterator Matrix<T>::end() const {
         const_iterator it(this->dimensions, dimensions.getCol() * dimensions.getRow(), this->matrix);
         return it;
     }
 
+    /**
+     * iterat over the elements of the matrix and apply functor on them
+     * @tparam Function
+     * @return
+     */
     template<class T>
     template<class Function>
     Matrix<T> Matrix<T>::apply(Function func) const {
