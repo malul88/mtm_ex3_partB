7333807f67e83f2417f35f1887b8b6668d57bbf2
malul88 <malul88>
2020-06-30 20:28:32 +0300

    init

---
+++ cmake-build-debug/CMakeFiles/mtm_ex3_partB.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mtm_ex3_partB.dir/Auxiliaries.cpp.obj"
+  "CMakeFiles/mtm_ex3_partB.dir/Matrix.cpp.obj"
+  "CMakeFiles/mtm_ex3_partB.dir/test_partB.cpp.obj"
+  "libmtm_ex3_partB.dll.a"
+  "mtm_ex3_partB.exe"
+  "mtm_ex3_partB.exe.manifest"
+  "mtm_ex3_partB.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mtm_ex3_partB.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
